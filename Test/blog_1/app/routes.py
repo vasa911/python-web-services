a9c7f9a8d8395140a7815f5c021cd1ac692b732b
vasa911 <vadym.vasylyev@outlook.com>
2020-07-25 15:46:46 +0300

    crud implemented

--- Test/blog_1/app/routes.py
+++ Test/blog_1/app/routes.py
@@ -8,6 +8,7 @@ from . import schemas
 
 post_schema = schemas.PostSchema()
 
+
 class PortListApi(Resource):
 
     def post(self):
@@ -20,9 +21,42 @@ class PortListApi(Resource):
         db.session.commit()
         return post_schema.dump(post), 201
 
-
     def get(self):
-        return {"message": "Hello"}
+        posts = db.session.query(models.Post).all()
+        return post_schema.dump(posts, many=True)
+
+
+class PostApi(Resource):
+    def get(self, uuid):
+        post = db.session.query(models.Post).filter_by(uuid=uuid).first()
+        if post is None:
+            return "", 404
+        return post_schema.dump(post)
+
+    def put(self, uuid):
+        post = db.session.query(models.Post).filter_by(uuid=uuid).first()
+        if post is None:
+            return "", 404
+
+        try:
+            post = post_schema.load(request.json, instance=post, session=db.session)
+        except ValidationError as e:
+            return {'message': str(e)}, 400
+        
+        db.session.add(post)
+        db.session.commit()
+
+        return post_schema.dump(post)
+    
+    def delete(self, uuid):
+        post = db.session.query(models.Post).filter_by(uuid=uuid).first()
+        if post is None:
+            return "", 404
+        db.session.delete(post)
+        db.session.commit()
+        return "", 204
+
 
 
 api.add_resource(PortListApi, '/posts')
+api.add_resource(PostApi, '/posts/<uuid>')
--- Test/blog_1/tests/test_post.py
+++ Test/blog_1/tests/test_post.py
@@ -1,6 +1,15 @@
 import json
+import datetime
 from unittest import mock
 from app import app
+
+
+class MockPost:
+    def __init__(self):
+        self.title = "Title1"
+        self.body = ""
+        self.date = datetime.datetime.strptime(
+            "2020-04-01T00:00", "%Y-%m-%dT%H:%M")
 
 
 def test_create_post():
@@ -19,3 +28,43 @@ def test_create_post():
 
     assert r.status_code == 201
     assert r.json['title'] == "Title1"
+
+
+def test_read_all_post():
+    with mock.patch('app.db.session.query') as query:
+        query.return_value.all.return_value = []
+        r = app.test_client().get("/posts")
+    assert r.status_code == 200
+    assert len(r.json) == 0
+
+
+def test_read_post():
+    with mock.patch("app.db.session.query") as query:
+        query.return_value.filter_by.return_value.first.return_value = MockPost()
+        r = app.test_client().get("/posts/123")
+    assert r.status_code == 200
+    assert r.json["title"] == "Title1"
+
+
+def test_update_post():
+    with mock.patch("app.db.session.query") as query, \
+            mock.patch("app.db.session.add") as add, \
+            mock.patch("app.db.session.commit") as commit:
+        query.return_value.filter_by.return_value.first.return_value = MockPost()
+        r = app.test_client().put(
+            "/posts/123",
+            data=json.dumps({"title": "Title2"}),
+            content_type="application/json"
+        )
+    r.status_code == 200
+    r.json['title'] == "Title2"
+
+
+def test_delete_post():
+    with mock.patch("app.db.session.query") as query, \
+            mock.patch("app.db.session.delete") as delete, \
+            mock.patch("app.db.session.commit") as commit:
+        query.return_value.filter_by.return_value.first.return_value = MockPost()
+        r = app.test_client().delete("/posts/123")
+    
+    assert r.status_code == 204
