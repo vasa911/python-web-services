4e1fc4f656e79de7aa5e3df4e94ecb41b2e6f0c4
vasa911 <vadym.vasylyev@outlook.com>
2020-07-25 14:52:30 +0300

    Finish unit test

--- Test/blog_1/tests/test_post.py
+++ Test/blog_1/tests/test_post.py
@@ -1,11 +1,21 @@
-import json 
+import json
+from unittest import mock
 from app import app
 
+
 def test_create_post():
-    app.test_client().post('/posts', data=json.dumps(
-        {
-            "title": "Title1",
-            "body": "sdgdsaf",
-            "date": "2018-04-01T00:00"
-        }, content_type="application/json"
-    ))
+    with mock.patch("app.db.session.add") as add, \
+            mock.patch("app.db.session.commit") as commit:
+        r = app.test_client().post('/posts', data=json.dumps(
+            {
+                "title": "Title1",
+                "body": "sdgdsaf",
+                "date": "2018-04-01T00:00"
+            }), content_type="application/json"
+        )
+
+        add.assert_called_once()
+        commit.assert_called_once()
+
+    assert r.status_code == 201
+    assert r.json['title'] == "Title1"
